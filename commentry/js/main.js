24b1f3838d6a4f0d1eb40252eda9d0e6b1a17994
Madhuraman Ghimire <madhuramanghimire@gmail.com>
2021-05-20 08:57:24 +0545

    Section A Array & object

--- commentry/js/main.js
+++ commentry/js/main.js
@@ -1,15 +1,23 @@
-$(function(){
-   
-})
-
+let commentCollection = [] // array
 $('#commentBox').keyup(function(e){
     let code = e.which
     if(code == 13){
         let newComment = $(this).val()
         var dt = new Date();
         var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-        let li = `<li> <span>`+ time +`</span>` + newComment + `</li>`
-        $('.display-wrapper ul').prepend(li)
+        let newCommentObject = {} // object
+        newCommentObject['time'] = time
+        newCommentObject['comment'] = newComment
+        commentCollection.push(newCommentObject)
         $(this).val('')
+        displayComment()
     }
 })
+
+function displayComment(){
+    $('.display-wrapper ul').html('')
+    commentCollection.map(function(comment){
+        let li = `<li> <span>`+ comment['time'] +`</span>` + comment['comment'] + `</li>`
+        $('.display-wrapper ul').prepend(li)
+    })
+}
