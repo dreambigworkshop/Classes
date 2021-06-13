97e270356e60888b610bade034f5c37954f1fd15
Madhuraman Ghimire <madhuramanghimire@gmail.com>
2021-06-13 08:52:18 +0545

    Form example

--- API/js/main.js
+++ API/js/main.js
@@ -1,37 +1,17 @@
 $('#searchBtn').click(function(){
     let movieName = $('#searchInput').val()
-    let serachResult = []
-    if(movieName){
-        let url = `http://api.tvmaze.com/search/shows?q=` + movieName
-        $.ajax({
-            method: "GET",
-            url: url,
-            success: function(res){
-                res.map(function(data){
-                    let movieObj = {}
-                    console.log(data.show)
-                    movieObj['name'] = data.show.name
-                    movieObj['image'] = data.show.image ? data.show.image.medium : 'img/sample.jpg'
-                    serachResult.push(movieObj)
-                })
-                displayMovie(serachResult)
-            },
-            error: function(err){
-                console.log('Got Error', err)
-            }
-        })
-    }
+    let movieList = []
+    let url = 'http://api.tvmaze.com/search/shows?q=' + movieName
+    $.ajax({
+        method: 'GET',
+        url: url,
+        success: function(res){
+            movieList = res
+        },
+        error: function(err){
+            console.log('Got Error', err)
+        }
+    })
 })
 
-function displayMovie(serachResult){
-    let movieCard = ``;
-    $('.result-wrapper').html('')
-    serachResult.map(function(movie){
-        movieCard = `<div class="movie-card">
-        <img src="`+movie['image']+`"/>
-        <p>`+movie['name']+`</p>
-        </div>`;
-
-        $('.result-wrapper').append(movieCard)
-    })
-}
+function displayMo
