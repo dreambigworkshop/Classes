98cd9e67388036e18742ccc9b8d1a5892b3a2240
Madhuraman Ghimire <madhuramanghimire@gmail.com>
2021-06-13 08:51:30 +0545

    Form Example

--- React/myfirstapp/src/App.js
+++ React/myfirstapp/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import HeadeComponent from './Components/HeadeComponent';
 import ContentComponent from './Components/ContentComponent';
 import FooterComponent from './Components/FooterComponent';
+import FormComponent from './Components/FormComponent';
 
 function App() {
   return (
     <div className="App">
-        <div className="header">
-          {/* <HeadeComponent /> */}
-        </div>
+        {/* <div className="header">
+          <HeadeComponent />
+        </div> */}
         <div className="content">
-          <ContentComponent initialValue={5}/>
+          {/* <ContentComponent initialValue={5}/> */}
+          <FormComponent />
         </div>
-        <div className="footer">
-          {/* <FooterComponent /> */}
-        </div>
+        {/* <div className="footer">
+          <FooterComponent />
+        </div> */}
     </div>
   );
 }
---
+++ React/myfirstapp/src/Components/FormComponent.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+}
+const taskInitial = {
+    tast: '',
+    data: '',
+    isFinished: ''
+}
+export default function FormComponent() {
+  const [values, setValues] = useState(initialValues);
+  const [formData, setFormData] = useState([]);
+  const handleChange = (e) => {
+    // console.log(e)
+    // console.log(e.target.name)
+    // console.log(e.target.value)
+    setValues((values) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const submitData = (e) => {
+      e.preventDefault()
+      setValues(initialValues)
+      let oldFormData = formData
+      oldFormData.push(values)
+      setFormData([...oldFormData])
+      console.log(formData)
+  }
+  return (
+    <div>
+      <form className="form" onSubmit={submitData}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          value={values.name}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={values.email}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={values.password}
+        />
+        <button type="submit">Submit</button>
+      </form>
+        <div className="results">
+            <ul>
+                {formData && formData.map((fd, key) => (
+                    <li key={key}>{fd.name} : {fd.email} : {fd.password}</li>
+                ))}
+            </ul>
+        </div>
+    </div>
+  );
+}
